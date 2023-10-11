47f0b147f05eb31a4d888ff2865bb23b04c33e64
Sarvesh15a <91020312+Sarvesh15a@users.noreply.github.com>
2023-10-11 22:45:29 +0530

    Add files via upload

---
+++ MyPortfolio/js/script.js
@@ -0,0 +1,7 @@
+
+var typed=new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","full Stack Web Developer","Java Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
